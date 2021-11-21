57f73d3197e5c06f17e2e2b9047253b122d9997d
YauheniZhukovets <92811758+YauheniZhukovets@users.noreply.github.com>
2021-11-21 17:46:42 +0300

    06(CallBack)

--- src/06/06_06.tsx
+++ src/06/06_06.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {MouseEvent} from 'react';
 
 
 // const callback = () => {
@@ -9,18 +9,30 @@ import React from 'react';
 
 export const User = () => {
 
-    const deleteUser = () => {
-        alert('user should be deleted')
+    const deleteUser = (event: MouseEvent<HTMLButtonElement>) => {
+        alert(event.currentTarget.name)
+        //alert('user should be deleted')
     }
 
-    const saveUser = () => {
-        alert('user should be saved')
+    // const saveUser = () => {
+    //     alert('user should be saved')
+    // }
+
+    const onNameChanged = () => {
+        console.log('name changed')
+    }
+
+    const focusLostHandler = () => {
+        console.log('focus last')
     }
 
 
-
-    return <div>Dimych
-        <button onClick={deleteUser}>delete</button>
-        <button onClick={saveUser}>save</button>
+    return <div><textarea
+        onChange={onNameChanged}
+        onBlur={focusLostHandler}
+    >Dimych</textarea>
+        <div><input type="Yo"/></div>
+        <button name={'delete'} onClick={deleteUser}>x</button>
+        <button name={'save'} onClick={deleteUser}>x</button>
     </div>
 }
