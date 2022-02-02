b94ca5d22b68104c468605fc1604bd14517d285d
YauheniZhukovets <92811758+YauheniZhukovets@users.noreply.github.com>
2022-02-02 23:39:40 +0300

    add component Promise

---
+++ src/14 Promise/Promise.js
@@ -0,0 +1,19 @@
+
+//Примеры async/await
+
+function wait (ms) {
+    return new Promise( (res) => {
+        setTimeout( (  )=>{ res() },ms )
+    } )
+}
+
+async function run () {
+    await wait(1000)
+    console.log(1)
+    await wait(2000)
+    console.log(2)
+    await wait(3000)
+    console.log(3)
+}
+
+run()
